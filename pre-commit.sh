3062a01f8de30979c367a2ec0dc473a9082febc8
Orestis Floros <orestisf1993@gmail.com>
2019-02-14 18:11:43 +0200

    pre-commit.sh: Use git checkout-index --temp

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 function check_fmt() {
-    if [[ $1 =~ \.yaml$ ]]; then
-        prettier --check --parser yaml || exit $?
-    elif [[ $1 =~ \.json$ ]]; then
-        prettier --check --parser json || exit $?
-    elif [[ $1 =~ \.pyi?$ ]]; then
-        tee >([[ "$(isort --check-only --multi-line=3 --trailing-comma --force-grid-wrap=0 \
-            --use-parentheses --line-width=88 -q -df -)" ]]) |
-            black --check --pyi --py36 -q - || exit $?
-    elif [[ $1 =~ \.sh$ ]]; then
-        shfmt -d -s -i 4 || exit $?
+    # No extension
+    if ! [[ $1 =~ \..*$ ]]; then
+        return
     fi
+
+    tmpdir=$(mktemp -d)
+    git checkout-index --prefix "$tmpdir/" "$1"
+    nf="$tmpdir/$1"
+    echo "$nf"
+
+    if [[ $1 =~ \.yaml$ ]]; then
+        prettier --check --parser yaml "$nf"
+    elif [[ $1 =~ \.json$ ]]; then
+        prettier --check --parser json "$nf"
+    elif [[ $1 =~ \.pyi?$ ]]; then
+        isort --check-only --multi-line=3 --trailing-comma --force-grid-wrap=0 \
+            --use-parentheses --line-width=88 -q -df "$nf" && black --check --py36 "$nf"
+    elif [[ $1 =~ \.sh$ ]]; then
+        shfmt -d -s -i 4 "$nf"
+    fi
+    exit_code=$?
+
+    rm -rf "$tmpdir"
+
+    return "$exit_code"
 }
 
 if ! [[ "$VIRTUAL_ENV" ]]; then
@@ -21,7 +35,7 @@ fi
 # lowercase 'd' filter means exclude deleted
 git diff --cached --name-only --diff-filter=d | while read FILE; do
     echo "$FILE"
-    git show ":$FILE" | check_fmt "$FILE" || exit $?
+    check_fmt "$FILE" || exit $?
 done || exit $?
 
 chronic python -m pytest -vv -s $(git ls-files 'test*.py')
