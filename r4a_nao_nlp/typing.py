e2e0ddb782c4e088a7d7aeef72ca4e81e61aaaa8
Orestis Floros <orestisf1993@gmail.com>
2019-02-25 12:48:07 +0200

    typing.py: assert TYPE_CHECKING

--- r4a_nao_nlp/typing.py
+++ r4a_nao_nlp/typing.py
@@ -3,11 +3,13 @@
 Contains various commonly used types. Can be used in combination with
 typing.TYPE_CHECKING to avoid loading spacy early.
 """
-from typing import Any, Dict
+from typing import TYPE_CHECKING, Any, Dict
 
 from spacy.tokens.doc import Doc
 from spacy.tokens.span import Span
 from spacy.tokens.token import Token
+
+assert TYPE_CHECKING
 
 # TODO: document what we use this type for: Can be converted to human-readable string and
 # to an eobject.
